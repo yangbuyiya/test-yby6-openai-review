87b89b01977e892371df5ecd022a505811cf5448
yangs <1692700664@qq.com>
2024-08-04 14:24:24 +0800

    feat 评审v4

--- test/src/main/java/com/yby6/Application.java
+++ test/src/main/java/com/yby6/Application.java
@@ -34,6 +34,86 @@ public class Application {
         while (true) {
             // do nothing
         }
+        // 内存溢出
+        List<byte[]> byteList = new ArrayList<>();
+        while (true) {
+            try {
+                byte[] bytes = new byte[Integer.MAX_VALUE];
+                byteList.add(bytes);
+            } catch (OutOfMemoryError e) {
+                System.out.println("内存溢出");
+                break;
+            }
+        }
+        System.out.println(byteList.size());
+        
+        while (true) {
+            // do nothing
+        }
+        // 内存溢出
+        List<byte[]> byteList = new ArrayList<>();
+        while (true) {
+            try {
+                byte[] bytes = new byte[Integer.MAX_VALUE];
+                byteList.add(bytes);
+            } catch (OutOfMemoryError e) {
+                System.out.println("内存溢出");
+                break;
+            }
+        }
+        System.out.println(byteList.size());
+        
+        while (true) {
+            // do nothing
+        }  // 内存溢出
+        List<byte[]> byteList = new ArrayList<>();
+        while (true) {
+            try {
+                byte[] bytes = new byte[Integer.MAX_VALUE];
+                byteList.add(bytes);
+            } catch (OutOfMemoryError e) {
+                System.out.println("内存溢出");
+                break;
+            }
+        }
+        System.out.println(byteList.size());
+        
+        while (true) {
+            // do nothing
+        }
+        // 内存溢出
+        List<byte[]> byteList = new ArrayList<>();
+        while (true) {
+            try {
+                byte[] bytes = new byte[Integer.MAX_VALUE];
+                byteList.add(bytes);
+            } catch (OutOfMemoryError e) {
+                System.out.println("内存溢出");
+                break;
+            }
+        }
+        System.out.println(byteList.size());
+        
+        while (true) {
+            // do nothing
+        }
+        // 内存溢出
+        List<byte[]> byteList = new ArrayList<>();
+        while (true) {
+            try {
+                byte[] bytes = new byte[Integer.MAX_VALUE];
+                byteList.add(bytes);
+            } catch (OutOfMemoryError e) {
+                System.out.println("内存溢出");
+                break;
+            }
+        }
+        System.out.println(byteList.size());
+        
+        while (true) {
+            // do nothing
+        }
+        
         
         /*
         * 要避免Java程序中的内存泄漏，可以采取以下措施：
