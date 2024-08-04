b21567a6af434884d21751ae46c1106f4ed39132
yangs <1692700664@qq.com>
2024-08-04 14:32:00 +0800

    feat 评审v6

--- test/src/main/java/com/yby6/Application.java
+++ test/src/main/java/com/yby6/Application.java
@@ -20,26 +20,12 @@ public class Application {
         
         // 内存溢出
         List<byte[]> byteList = new ArrayList<>();
-        while (true) {
-            try {
-                byte[] bytes = new byte[Integer.MAX_VALUE];
-                byteList.add(bytes);
-            } catch (OutOfMemoryError e) {
-                System.out.println("内存溢出");
-                break;
-            }
-        }
-        System.out.println(byteList.size());
-        
-        while (true) {
-            // do nothing
-        }
-        System.out.println("Hello World Code Review");
-        
-        System.out.println(Integer.parseInt("ddd"));
-        // https://github.com/yangbuyiya/test-yby6-openai-review
-        
-        // 内存溢出
+        List<byte[]> byteList = new ArrayList<>();
+        List<byte[]> byteList = new ArrayList<>();
+        List<byte[]> byteList = new ArrayList<>();
+        List<byte[]> byteList = new ArrayList<>();
+        List<byte[]> byteList = new ArrayList<>();
+        List<byte[]> byteList = new ArrayList<>();
         List<byte[]> byteList = new ArrayList<>();
         while (true) {
             try {
@@ -55,29 +41,6 @@ public class Application {
         while (true) {
             // do nothing
         }
-        
-        System.out.println("Hello World Code Review");
-        
-        System.out.println(Integer.parseInt("ddd"));
-        // https://github.com/yangbuyiya/test-yby6-openai-review
-        
-        // 内存溢出
-        List<byte[]> byteList = new ArrayList<>();
-        while (true) {
-            try {
-                byte[] bytes = new byte[Integer.MAX_VALUE];
-                byteList.add(bytes);
-            } catch (OutOfMemoryError e) {
-                System.out.println("内存溢出");
-                break;
-            }
-        }
-        System.out.println(byteList.size());
-        
-        while (true) {
-            // do nothing
-        }
-        
         
         /*
         * 要避免Java程序中的内存泄漏，可以采取以下措施：
